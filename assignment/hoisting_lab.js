8d6af8ca62f164aa8e1d0391e210f5c74b69347c
JAI CHAUDHARY <chaudharyjai6269@gmail.com>
2025-11-18 10:19:34 +0530

    Update hoisting_lab.js

---
+++ assignment/hoisting_lab.js
@@ -0,0 +1,9 @@
+const announce = () => console.log("Game started");
+const startGame = () => console.log(status);
+
+var score = 50;
+let status = "ready";
+
+console.log(score);
+announce();
+startGame();
